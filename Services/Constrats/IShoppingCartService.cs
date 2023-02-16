e2792907ad74cad163460418a6fbd9f02e66e601
XiaocongZheng <zhengglamorgan@gmail.com>
2023-02-16 00:32:45 -0500

    Add project files.

---
+++ Services/Constrats/IShoppingCartService.cs
@@ -0,0 +1,12 @@
+﻿using UnitTest.Models;
+
+namespace UnitTest.Services.Constrats
+{
+    public interface IShoppingCartService
+    {
+        IEnumerable<ShoppingItem> GetAllItems();
+        ShoppingItem Add(ShoppingItem newItem);
+        ShoppingItem GetById(Guid id);
+        void Remove(Guid id);       
+    }
+}
---
+++ Services/ShoppingCartService.cs
@@ -0,0 +1,13 @@
+﻿using UnitTest.Models;
+using UnitTest.Services.Constrats;
+
+namespace UnitTest.Services
+{
+    public class ShoppingCartService: IShoppingCartService
+    {
+        public ShoppingItem Add(ShoppingItem newItem) => throw new NotImplementedException();
+        public IEnumerable<ShoppingItem> GetAllItems() => throw new NotImplementedException();
+        public ShoppingItem GetById(Guid id) => throw new NotImplementedException();
+        public void Remove(Guid id) => throw new NotImplementedException();
+    }
+}
